7e95159a80af651b2a6270403a189f4f79619af0
Ali Nafisi <74741200+safinal@users.noreply.github.com>
2025-04-16 15:11:27 +0330

    Update DetaiIntroduction.js

--- src/components/introduction/DetaiIntroduction.js
+++ src/components/introduction/DetaiIntroduction.js
@@ -15,13 +15,15 @@ const DetaiIntroduction = (props) => {
                 <p className="text-gray text-2xl xl:text-3xl 3xl:text-5xl font-medium mb-3 xl:mb-4 hidden md:block">Hello! I’m</p>
                 <h2 className="text-gray-dark text-3xl xl:text-4xl 3xl:text-6xl font-black hidden md:block">Ali Nafisi</h2>
                 <p className="text-gray-dark text-sm md:text-base 2xl:text-lg 3xl:text-xl text-justify mt-[10rem] md:mt-4 xl:mt-6 3xl:mt-10">
-                    I am an MS student in Artificial Intelligence at Bu-Ali Sina University and a graduate student researcher at <a className="text-blue-500" href="https://rivlab.github.io/">Robot Intelligence & Vision Lab</a>, led by <a className="text-blue-500" href="https://scholar.google.com/citations?hl=en&user=5YX31NgAAAAJ">Prof. Hassan Khotanlou</a>. I completed my BS in Computer Engineering in 2023 at University of Kurdistan.
+                    I’m a Master’s student in Artificial Intelligence at Bu-Ali Sina University and a graduate researcher at the <a className="text-blue-500" href="https://rivlab.github.io/">Robot Intelligence & Vision Lab</a>, under the supervision of <a className="text-blue-500" href="https://scholar.google.com/citations?hl=en&user=5YX31NgAAAAJ">Prof. Hassan Khotanlou</a>. I earned my BSc in Computer Engineering from the University of Kurdistan in 2023.
                 </p>
                 <p className=" text-gray-dark text-sm md:text-base 2xl:text-lg 3xl:text-xl text-justify mt-2 xl:mt-3 3xl:mt-5">
-                    I have broad interests in Deep Learning, with a particular focus on its applications in Trustworthy AI and Multimodal Generative Models. I am also passionate about Deep Learning-Guided Program Synthesis, aiming to develop intelligent systems capable of true invention, adaptation, and innovation. Additionally, I have a strong interest in Deep Reinforcement Learning, which I believe holds great potential for pushing the boundaries of AI beyond human-level performance across various domains. My current research centers on Debiasing Image Datasets with Generative Models.
+                    I have broad interests in Deep Learning, with a particular focus on its applications in Trustworthy AI and Multimodal Generative Models. I am also passionate about Deep Learning-Guided Program Synthesis, aiming to develop intelligent systems capable of true invention, adaptation, and innovation. Additionally, I have a strong interest in Deep Reinforcement Learning, which I believe holds great potential for pushing the boundaries of AI beyond human-level performance across various domains.
+                </p>
+                    Currently, my research is focused on Debiasing Image Datasets using Generative Models, addressing critical issues of fairness and reliability in AI systems.
                 </p>
                 <p className="text-gray-dark text-sm md:text-base 2xl:text-lg 3xl:text-xl text-justify mt-2 xl:mt-3 3xl:mt-5">
-                    Feel free to reach out. I'm open to collaborations!
+                    I’m always open to collaboration and connecting with others in the field. Feel free to reach out!
                 </p>
                 <EducationButton showEducation={showEducation} setShowEducation={setShowEducation} />
                 <div className={`${showEducation ? 'opacity-100' : 'opacity-0'} transition-opacity ease-in duration-700`}>
